662158b103bf98f12889b4dae475f3de27c3e003
Meghana Anupoju <meghana.anupoju@example.com>
2026-07-14 19:24:09 +0530

    fix: remove github pages base url for vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Smart-Stadiums-Tournament-Operations/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
